50364e46ecce0a665feaff470bbd5bbed6b91220
mechweb <mechsharad21@gmail.com>
2023-01-08 13:14:46 +0530

    create heading

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * <div class="header">
+        <h1>Namaste React</h1>
+        <ul>
+            <li>About Us</li>
+            <li>Support</li>
+            <li>Home</li>
+        </ul>
+    </div>
+    creating this with react
+ */
+
+  const root=ReactDOM.createRoot(document.getElementById("root"));
+  const header=React.createElement(
+    "div",
+    {},
+    "hello js"
+  )
+  
+  root.render(header);
+  console.log(header);
