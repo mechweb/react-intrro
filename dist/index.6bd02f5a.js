55e8fd13c320ab99d46d7b6b8fa303fed5dc0750
mechweb <mechsharad21@gmail.com>
2023-01-10 18:34:39 +0530

    chore: build files

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,16 @@
+/**
+ * <div class="header">
+        <h1>Namaste React</h1>
+        <ul>
+            <li>About Us</li>
+            <li>Support</li>
+            <li>Home</li>
+        </ul>
+    </div>
+    creating this with react
+ */ const root = ReactDOM.createRoot(document.getElementById("root"));
+const header = React.createElement("div", {}, "hello js");
+root.render(header);
+console.log(header);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
